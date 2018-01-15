6cdc0e01c2edaea6d084b5072523dfeee0f36ef3
bulve <barankarankis@gmail.com>
2018-01-15 10:46:34 +0200

    Swagger changes..

--- iSveikata-app/src/main/java/lt/vtvpmc/ems/isveikata/SwaggerConfig.java
+++ iSveikata-app/src/main/java/lt/vtvpmc/ems/isveikata/SwaggerConfig.java
@@ -3,6 +3,7 @@ package lt.vtvpmc.ems.isveikata;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
