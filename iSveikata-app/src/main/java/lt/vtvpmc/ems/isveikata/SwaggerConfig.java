ee3df22bbd8eb1ea4ce3e23c1f3e36a81e707783
Jonas <32129784+JKaspas@users.noreply.github.com>
2018-01-15 09:44:16 +0200

    Update SwaggerConfig.java

--- iSveikata-app/src/main/java/lt/vtvpmc/ems/isveikata/SwaggerConfig.java
+++ iSveikata-app/src/main/java/lt/vtvpmc/ems/isveikata/SwaggerConfig.java
@@ -11,7 +11,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
-@SpringBootApplication
+@Configuration
 public class SwaggerConfig{
     @Bean
     public Docket SwaggerDocket(){
