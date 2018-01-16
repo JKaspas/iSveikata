ecda4717236a7f25a425f112b08e76ae4e648186
Giovanni <giovanni@blabla.bla>
2018-01-16 12:15:29 +0200

    updated

--- iSveikata-app/src/main/java/lt/vtvpmc/ems/isveikata/patient/PatientController.java
+++ iSveikata-app/src/main/java/lt/vtvpmc/ems/isveikata/patient/PatientController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import lt.vtvpmc.ems.isveikata.medical_record.MedicalRecord;
 
 @RestController
 @RequestMapping(value = "/api/patient")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PatientController {
 
 	@Autowired
