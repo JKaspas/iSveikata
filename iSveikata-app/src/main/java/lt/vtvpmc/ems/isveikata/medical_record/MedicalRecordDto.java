79fcfd534ce1a8c096d4131e1d22a07a1329120d
Giovanni <giovanni@blabla.bla>
2018-02-06 22:45:33 +0200

    some dto changes

--- iSveikata-app/src/main/java/lt/vtvpmc/ems/isveikata/medical_record/MedicalRecordDto.java
+++ iSveikata-app/src/main/java/lt/vtvpmc/ems/isveikata/medical_record/MedicalRecordDto.java
@@ -1,21 +1,27 @@
 package lt.vtvpmc.ems.isveikata.medical_record;
 
+import java.util.Date;
+
 import lombok.Data;
-import lt.vtvpmc.ems.isveikata.appointment.Appointment;
-import lt.vtvpmc.ems.isveikata.employees.Doctor;
-import lt.vtvpmc.ems.isveikata.icd.Icd;
-import lt.vtvpmc.ems.isveikata.patient.Patient;
 
 @Data
 public class MedicalRecordDto {
-	
-//	private Long id;
-//	private Doctor doctor;
-//	private Long patientId;
-//	private Appointment appointment;
-//	private Icd icd;
+
+	// private Long id;
+	// private Doctor doctor;
+	// private Long patientId;
+	// private Appointment appointment;
+	// private Icd icd;
+
+	private String doctorFullName;
+	private String icdCode;
+	private String icdDescription;
+	private Date appointmentDate;
+	private Integer appoitmentDuration;
+	private String appointmentDescription;
 	private boolean isCompensable;
 	private boolean isRepetitive;
 
-	
+	// appointmentDate, doctorFullName, icdCode, icdTitle, appointmentDuration,
+	// description, compensable, isRepetitive
 }
