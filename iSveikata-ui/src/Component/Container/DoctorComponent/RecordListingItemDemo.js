e01381761995647a227db2a7c4213d0b083acde7
bulve <barankarankis@gmail.com>
2018-02-22 11:45:21 +0200

    small chnages...

--- iSveikata-ui/src/Component/Container/DoctorComponent/RecordListingItemDemo.js
+++ iSveikata-ui/src/Component/Container/DoctorComponent/RecordListingItemDemo.js
@@ -5,7 +5,7 @@ import React from 'react'
 const RecordListingItemDemo = (props) =>{
 
     return (
-    <tr  data-toggle="modal" data-target="#myModal" onClick={() => props.showDetails(props.id)} >
+    <tr  data-toggle="modal" data-backdrop="false"  data-target="#myModal" onClick={() => props.showDetails(props.id)} >
             <td>{props.appDate}</td>
             <td>{props.doctorName}</td>
             <td>{props.icd}</td>
