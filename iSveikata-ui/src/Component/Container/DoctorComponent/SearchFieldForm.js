bdf251f5f67b0d5ef7e30d45a9616a398622168d
Giovanni <giovanni@blabla.bla>
2018-03-06 20:06:26 +0200

    search form

--- iSveikata-ui/src/Component/Container/DoctorComponent/SearchFieldForm.js
+++ iSveikata-ui/src/Component/Container/DoctorComponent/SearchFieldForm.js
@@ -3,15 +3,16 @@ import React from 'react'
 var SearchFieldForm = (props) =>{
     return(
     <form onSubmit={props.searchHandler} className="form-inline text-center">
-        <div className="form-group ">     
+        <div className="form-group input-append">     
             <input id="searchFormInput" onKeyUp={props.searchHandler} type={props.searchType} className="form-control" value={props.searchValue} onChange={props.fielddHandler} placeholder={props.searchPlaceHolder} name="userName" />
-        </div>
-        <div className="form-group">     
+        {/* </div>
+        <div className="form-group">      */}
             <button id="searchFormSubmit" type="submit" className="btn btn-default" >
             <i className="glyphicon glyphicon-search"></i>
             </button>
         </div>
-    </form>)
+    </form>
+    )
 }
 
 export default SearchFieldForm
